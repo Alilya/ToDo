1082e408a4cc7f7502bb98571a0d0f9dcde985e6
Alilya <alinakolesnikova0101@gmail.com>
2023-10-05 21:44:16 +0300

    sass styles

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import style from "./Menu.module.sass"
+import style from "./Menu.module.sass";
 
-const Menu=()=>{
-    return (
-      <div className={style.menu}>
-        <NavLink to="/task" className={style.menu_nav}> Задания </NavLink>
-        <NavLink to="/projects" className={style.menu_nav}> Проекты </NavLink>
-      </div>
-    );
-}
+const Menu = () => {
+  return (
+    <div className={style.menu}>
+      <NavLink to="/task" className={style.menu_nav}> Задания </NavLink>
+      <NavLink to="/projects" className={style.menu_nav}> Проекты </NavLink>
+    </div>
+  );
+};
 export default Menu;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.sass';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 import ToDo from './App';
 
