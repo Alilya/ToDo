22ea6f1e40450b6d804d9d4ce407cad075a4fd83
Alilya <alinakolesnikova0101@gmail.com>
2023-10-12 22:28:56 +0300

    correct local store

--- src/Components/Project/Modal.jsx
+++ src/Components/Project/Modal.jsx
@@ -3,33 +3,49 @@ import style from "./Modal.module.sass";
 import useLocalStorageState from "use-local-storage-state";
 
 const ModalContent = (props) => {
-  const [text, setText] = useLocalStorageState('value', {defaultValue: "Новый проект"});
+  const [name, setName] = useLocalStorageState("newProject", {
+    defaultValue: '',
+  });
+
+  const [description, setDescription] = useLocalStorageState("description", {
+    defaultValue: '',
+  });
+
+  const [timeInWork, setTimeInWork] = useLocalStorageState("timeInWork", {
+    defaultValue: Date(0,0),
+  });
+  
+  const [priority, setPriority] = useLocalStorageState("priority", {
+    defaultValue: '',
+  });
+  const [status, setStatus] = useLocalStorageState("status", {
+    defaultValue: '',
+  });
+  
+  const [comment, setComment] = useLocalStorageState("comment", {
+    defaultValue: '',
+  });
 
   let addNewProject = (e) => {
-    console.log(text);
+    console.log(name);
   };
 
   return (
     <div>
       <form action="" className={style.createProject}>
-        Number
-        <input
-          type="text"
-          placeholder="Заголовок"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <input type="text" placeholder="Описание"/>
+        <input type="text" placeholder="Заголовок" value={name} onChange={(e) => setName(e.target.value)}/>
+        <input type="text" placeholder="Описание"  value={description} onChange={(e) => setDescription(e.target.value)}/>
         Дата создания
         <input type="date" />
-        <input type="text" placeholder="Время в работе" />
+        Время в работе
+        <input type="time" value={timeInWork} onChange={(e) => setTimeInWork(e.target.value)}/>
         Дата окончания
         <input type="date" />
-        <input type="text" placeholder="Приоритет" />
+        <input type="text" placeholder="Приоритет" value={priority}  onChange={(e) => setPriority(e.target.value)}/>
         Файл
         <input type="file" />
-        <input type="text" placeholder="Текущий статус" />
-        <input type="text" placeholder="Комментарии" />
+        <input type="text" placeholder="Текущий статус" value={status}  onChange={(e) => setStatus(e.target.value)}/>
+        <input type="comment" placeholder="Комментарий"  value={comment}  onChange={(e) => setComment(e.target.value)}/>
       </form>
       <button
         type="submit"
